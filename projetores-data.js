5df2851bbc3181b1f0e98ab7266f173c05a3b06c
schardt-commits <thaleschardt@gmail.com>
2026-07-31 18:39:05 -0300

    AC1085: tira do ar ate o video ficar pronto (variante errada no historico; volta com revert deste commit)

--- projetores-data.js
+++ projetores-data.js
@@ -399,7 +399,7 @@ window.PROJETORES_DATA = [
     "tecnologias": "",
     "bateria": false,
     "cap_bateria": "",
-    "video_url": "https://youtu.be/w5XFtD22ek8",
+    "video_url": "",
     "suporte_giratorio": true,
     "audio_qualidade": "",
     "uso_continuo": "4h",
@@ -415,9 +415,10 @@ window.PROJETORES_DATA = [
     "armazenamento_gb": 8,
     "status": "nacional",
     "link_importado": "",
-    "bolsa": ""
-  },
-  {
+    "bolsa": "",
+    "listar": false,
+    "so_ranking": true
+  },  {
     "marca": "Byintek",
     "modelo": "Love U4",
     "tecnologia": "LCD LED",
